80f8f8b6d7170ff305baeea4b913c4c424c021ab
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-01-02 00:47:14 +0530

    Update 1342. Number of Steps to Reduce a Number to Zero.py

--- The LeetCode Beginner's Guide/Challenge Problems/1342. Number of Steps to Reduce a Number to Zero.py
+++ The LeetCode Beginner's Guide/Challenge Problems/1342. Number of Steps to Reduce a Number to Zero.py
@@ -15,6 +15,19 @@ class Solution(object):
         return count
 
 """
-Time Complexity O(n)
+Time Complexity O(log n)
 Space Complexity O(1)
+"""
+
+"""
+We can further reduce the memory usage by using a recursive approach.
+"""
+class Solution(object):
+    def numberOfSteps(self, num):
+        if num == 0:
+            return 0
+        return 1 + self.numberOfSteps(num - 1 if num & 1 else num >> 1)
+"""
+Time Complexity O(logn)
+Space Complexity O(logn)
 """
