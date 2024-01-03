fdbb8ae9901c55bf02632fd8e18943cf0b8371a3
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-01-03 23:54:17 +0530

    Create Merge sort.py

---
+++ Sorting algorithms/Merge sort.py
@@ -0,0 +1,42 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        L = arr[:mid]
+        R = arr[mid:]
+
+        merge_sort(L)
+        merge_sort(R)
+
+        i = j = k = 0
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+    return arr
+
+"""
+Time Complexity:
+Average and Worst Case: 
+O(nlogn)
+Best Case: 
+O(nlogn)
+Space Complexity:
+Traditional implementation: 
+O(n)
+In-Place implementation: 
+O(1) but more complex
+"""
