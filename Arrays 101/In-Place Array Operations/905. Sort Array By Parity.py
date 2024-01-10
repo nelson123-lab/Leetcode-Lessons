8ca10c4443907dacb1c10ec6ff4b7780c14e9ef5
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-01-11 03:11:40 +0530

    Update 905. Sort Array By Parity.py

--- Arrays 101/In-Place Array Operations/905. Sort Array By Parity.py
+++ Arrays 101/In-Place Array Operations/905. Sort Array By Parity.py
@@ -17,3 +17,25 @@ class Solution(object):
 Time Complexity O(n)
 Space Complexity O(n)
 """
+
+# Solution with in-place operations.
+class Solution(object):
+    def sortArrayByParity(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] %2 == 0:
+                temp = nums[k]
+                nums[k] = nums[i]
+                nums[i] = temp
+                k += 1
+            else: pass
+        return nums
+
+"""
+Time Complexity O(n)
+Space Complexity O(1)
+"""
