f253274a663251bab6c3c5b8b44e265fa30dfcc4
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-01-12 23:52:39 +0530

    Create 414. Third Maximum Number.py

---
+++ Arrays 101/Conclusion/414. Third Maximum Number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        first, second, third = float('-inf'), float('-inf'), float('-inf')
+
+        for num in nums:
+            if num > first:
+                first, second, third = num, first, second
+            elif first > num > second:
+                second, third = num, second
+            elif second > num > third:
+                third = num
+
+        if third != float('-inf'):
+            return third
+        else:
+            return first
+
+  """
+  Time complexity O(n)
+  Space Complexity O(1)
+  """
