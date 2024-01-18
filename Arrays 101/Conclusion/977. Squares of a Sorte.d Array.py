5070ee8612fcb42183f69c8aecd00e4cc78d3b63
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-01-18 23:16:30 +0530

    Create 977. Squares of a Sorte.d Array.py

---
+++ Arrays 101/Conclusion/977. Squares of a Sorte.d Array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def sortedSquares(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            nums[i] *= nums[i]
+        nums.sort()
+        return nums
+
+"""
+Time Complexity O(n)
+Space Complexity O(n)
+"""
